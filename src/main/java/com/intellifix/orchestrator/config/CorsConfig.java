c85fbe96e2ed7a0dc203e1acd9055e3ac4edffa4
java_dev <apibackend.java@gmail.com>
2026-03-20 14:28:57 +0530

    Added CORS config

---
+++ src/main/java/com/intellifix/orchestrator/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.intellifix.orchestrator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://intellifix-ui-uat.s3-website-ap-south-1.amazonaws.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
